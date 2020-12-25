31c526dd3fa08af80574e30b022d4ef0a878b323
Filip <kopriv.filip@seznam.cz>
2020-12-25 13:48:27 +0100

    Simple server with nodemon

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Dress 1',
+            category: 'dress',
+            image: '/images/p1.jpg',
+            price: 2200,
+            countInStock: 20,
+            brand: 'sth',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'dress',
+        },
+        {
+            _id: '21',
+            name: 'Dress 2',
+            category: 'dress',
+            image: '/images/p2.jpg',
+            price: 2500,
+            countInStock: 5,
+            brand: 'sth',
+            rating: 4.8,
+            numReviews: 15,
+            description: 'dress',
+        },
+        {
+            _id: '3',
+            name: 'Dress 3',
+            category: 'dress',
+            image: '/images/p3.jpg',
+            price: 2300,
+            countInStock: 0,
+            brand: 'sth',
+            rating: 4.3,
+            numReviews: 13,
+            description: 'dress',
+        },
+        {
+            _id: '4',
+            name: 'Dress 4',
+            category: 'dress',
+            image: '/images/p4.jpg',
+            price: 2200,
+            countInStock: 12,
+            brand: 'sth',
+            rating: 3.8,
+            numReviews: 20,
+            description: 'dress',
+        },
+    ]
+}
+export default data;
