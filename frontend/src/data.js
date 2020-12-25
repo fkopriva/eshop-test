13577ff1d7cfffcae8541bbd70e211e01bc00bc4
Filip <kopriv.filip@seznam.cz>
2020-12-25 12:26:06 +0100

    share code on git

---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Dress 1',
+            category: 'dress',
+            image: '/images/p1.jpg',
+            price: 2200,
+            brand: 'sth',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'dress',
+        },
+        {
+            _id: '21',
+            name: 'Dress 2',
+            category: 'dress',
+            image: '/images/p2.jpg',
+            price: 2500,
+            brand: 'sth',
+            rating: 4.8,
+            numReviews: 15,
+            description: 'dress',
+        },
+        {
+            _id: '3',
+            name: 'Dress 3',
+            category: 'dress',
+            image: '/images/p3.jpg',
+            price: 2300,
+            brand: 'sth',
+            rating: 4.3,
+            numReviews: 13,
+            description: 'dress',
+        },
+        {
+            _id: '4',
+            name: 'Dress 4',
+            category: 'dress',
+            image: '/images/p4.jpg',
+            price: 2200,
+            brand: 'sth',
+            rating: 4.0,
+            numReviews: 20,
+            description: 'dress',
+        },
+    ]
+}
+export default data;
