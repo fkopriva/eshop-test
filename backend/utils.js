728bb9f4cd4730611079eca1076f6968daad60d9
Filip <kopriv.filip@seznam.cz>
2020-12-27 17:13:40 +0100

    order model

--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { reset } from 'nodemon';
 
 export const generateToken = (user) => {
     return jwt.sign({
@@ -11,4 +12,21 @@ export const generateToken = (user) => {
     {
         expiresIn: '30d',
     });
+};
+
+export const isAuth = (req, res, next) => {
+    const authorization = req.header.authorization;
+    if (authorization) {
+        const token = authorization.slice(7, authorization.length); // Bearer XXXXXX => start at 7
+        jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', (err, decode) => {
+            if (err) {
+                res.status(401).send({ message: 'Invalid Token' });
+            } else {
+                req.user = decode;
+                next();
+            }
+        });
+    } else {
+        res.status(401).send({ message: 'No Token' });
+    }
 };
