a03095549e4d57f4a61baba4f0da9a1cbd515c40
Filip <kopriv.filip@seznam.cz>
2020-12-27 15:57:18 +0100

    Shipping screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,13 +12,16 @@ import { userRegisterReducer, userSigninReducer } from './reducers/userReducers'
 const initialState = {
     userSignin: {
         userInfo: localStorage.getItem('userInfo')
-        ? JSON.parse(localStorage.getItem('userInfo'))
-        : null,
+            ? JSON.parse(localStorage.getItem('userInfo'))
+            : null,
     },
     cart: {
         cartItems:localStorage.getItem('cartItems')
-        ? JSON.parse(localStorage.getItem('cartItems')) 
-        : [],
+            ? JSON.parse(localStorage.getItem('cartItems')) 
+            : [],
+        shippingAddress: localStorage.getItem('shippingAddress')
+            ? JSON.parse(localStorage.getItem('shippingAddress'))
+            : {}, 
     },
 };
 const reducer = combineReducers({
