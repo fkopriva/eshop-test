42d745f6e463d97ea2a8f55a753737fd3e769a5b
Filip <kopriv.filip@seznam.cz>
2020-12-28 17:04:31 +0100

    payment process

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,6 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/eshop', {
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter)
-
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
